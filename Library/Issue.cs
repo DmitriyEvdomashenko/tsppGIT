d1973b694082e8d11e02c08739a16b8915f5db20
Dmitriy Evdomashenko <dimas54321121@gmail.com>
2019-05-25 13:05:31 +0300

    commit_4

--- Library/Issue.cs
+++ Library/Issue.cs
@@ -1,12 +1,21 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Library
 {
     public class Issue
     {
+        public Book objBook = new Book();
+        public Customer objCustomer = new Customer();
+        public DateTime date = new DateTime();
+        public Worker ObjWorker = new Worker();
+        public Issue(string _nameAu, string _cust, int _numCust, DateTime dat, string workName)
+        {
+            this.objBook.nameAuthor = _nameAu;
+            this.objCustomer.name = _cust;
+            this.objCustomer.num = _numCust;
+            this.date = dat;
+            this.ObjWorker.name = workName;
+        }
+        ~Issue() { }
     }
 }
--- Library/Worker.cs
+++ Library/Worker.cs
@@ -1,12 +1,37 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Library
 {
     public class Worker
     {
+        public BookCatalog _books = new BookCatalog();
+        public string name { get { string _name = "Работник Работникович"; return _name; } set { } }
+        public int ViewCatalog(ref BookCatalog _books) { _books.ShowListBook(); return 0; }
+        public int WriteOffBook(ref Book _book)
+        {
+            _book.state = "Списана";
+            return 0;
+        }
+        public int IssueBook(ref Book _book, Customer _person, ref IssueList _issueList)
+        {
+            _book.state = "Выдана";
+            _issueList.AddListIssue(new Issue(this.name, _person.name, _person.num, DateTime.Now, _book.title));
+            return 0;
+        }
+        // public int AddBook() => _books.AddBook();
+        public void ChangeNameWorker() => name = Console.ReadLine();
+        public void RetBook(ref BookCatalog _bc)
+        {
+            Console.WriteLine("Введите название книги которую хотите вернуть");
+            string title = Console.ReadLine();
+            int i = 0;
+            for (i = 0; i < _bc.book.Count; i++)
+            {
+                if (string.Compare(title, _bc.book[i].title) == 0) _bc.book[i].state = "Свободная";
+            }
+            //_book.state = "Свободная";
+            return;
+        }
+        //public void ShowInfWorker() { return ; }
     }
 }
