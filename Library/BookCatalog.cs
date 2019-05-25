9eebba66c1a024c164834925ce2d2d7acd54e3a4
Dmitriy Evdomashenko <dimas54321121@gmail.com>
2019-05-25 13:04:17 +0300

    commit_3

--- Library/BookCatalog.cs
+++ Library/BookCatalog.cs
@@ -1,12 +1,50 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace Library
 {
     public class BookCatalog
     {
+        public List<Book> book = new List<Book>();
+
+        public int SearchBook(Book _book)
+        {
+            bool fl = false;
+            Book itemSearch = new Book();
+            Console.WriteLine("Введите характеристику книги");
+            itemSearch.SetInfBook();
+            foreach (Book i in book)
+            {
+                if (i == itemSearch)
+                {
+                    fl = true;
+                    Console.WriteLine("Книга найдена: ");
+                    i.ShowInfBook();
+                }
+            }
+            if (fl == false) Console.WriteLine("Данной книги нет в каталоге");
+            //book.Sort();book.BinarySearch(itemSearch);
+            return 0;
+        }
+        public int Sort() { book.Sort(); Console.WriteLine("Список отсортирован"); return 0; }
+        public int AddBook()
+        {
+            Book itemAdd = new Book();
+            Console.WriteLine("Введите характеристику книги");
+            itemAdd.SetInfBook();
+            book.Add(itemAdd);
+            return 0;
+        }
+        public void ShowListBook()
+        {
+            for (int i = 0; i < book.Count; i++)
+            {
+                if (i == 0) book[i].ShowInfBook(true);
+                else book[i].ShowInfBook(false);
+            }
+            Console.WriteLine("______________________________________");
+        }
+        public int DeleteBook(Book _book) { return 0; }
     }
 }
--- Library/IssueList.cs
+++ Library/IssueList.cs
@@ -8,5 +8,23 @@ namespace Library
 {
     public class IssueList
     {
+        public List<Issue> stat = new List<Issue>();
+        public void ShowList()
+        {
+            for (int i = 0; i < stat.Count; i++)
+            {
+                Console.WriteLine("_________________________________________________________");
+                Console.WriteLine("+      Работник        |     Клиент/Номер     |        Дата          |       Книга        +");
+                Console.WriteLine($"+{stat[i].ObjWorker.name}  {stat[i].objCustomer.name},{stat[i].objCustomer.num}   {stat[i].date}{stat[i].objBook.title}+");
+            }
+            Console.WriteLine("_________________________________________________________");
+            return;
+        }
+        public int ChangeListIssue() { return 0; }
+        public int AddListIssue(Issue _issue = null)
+        {
+            stat.Add(_issue);
+            return 0;
+        }
     }
 }
